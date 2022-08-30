2775e5df67027e75c649320bf773f096b06f62fe
charuzain <charuabhizain@gmail.com>
2022-08-29 22:16:27 -0400

    Multiple pointer : Count no of unique value in an array

--- DSA/problem-solving-pattern/pointer/countUniqueValues.js
+++ DSA/problem-solving-pattern/pointer/countUniqueValues.js
@@ -37,6 +37,8 @@
 
 const countUniqueValues = function(arr) {
 
+  if (arr.length === 0) return 0;
+
   let i = 0;
   let j = i + 1;
 
@@ -62,6 +64,11 @@ const countUniqueValues = function(arr) {
 };
 
 console.log(countUniqueValues([1, 1, 1, 2, 2, 2, 3]));
+console.log(countUniqueValues([1, 1, 1, 1, 1, 2]));
+console.log(countUniqueValues([1, 1, 1, 1, 1, 2]));
+console.log(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]));
+console.log(countUniqueValues([]));
+console.log(countUniqueValues([-2,-1,-1,0,1]));
   
 
 
