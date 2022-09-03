6db4b0739ded06eb01da189402baaf28cc960d3d
charuzain <charuabhizain@gmail.com>
2022-09-03 09:16:56 -0400

    Bubble sort implementation

--- DSA/sorting/bubbleSort.js
+++ DSA/sorting/bubbleSort.js
@@ -16,11 +16,13 @@
 const bubbleSort = function(arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length - 1 - i; j++) {
+      // console.log(arr[j],arr[j + 1]);
       if (arr[j + 1] < arr[j]) {
         let temp = arr[j + 1];
         arr[j + 1] = arr[j];
         arr[j] = temp;
       }
+      console.log(arr);
     }
   }
   return arr;
