4a39ad0208c58e990ace39397a03a527f2421da8
charuzain <charuabhizain@gmail.com>
2022-09-03 00:38:54 -0400

    bubbleSort

--- DSA/sorting/bubbleSort.js
+++ DSA/sorting/bubbleSort.js
@@ -1,17 +1,28 @@
+// const bubbleSort = function(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[j] < arr[i]) {
+//         let smallerValue = arr[j];
+//         arr[j] = arr[i];
+//         arr[i] = smallerValue;
+//         console.log(arr);
+//       }
+//     }
+//   }
+  
+//   return arr;
+// };
+
 const bubbleSort = function(arr) {
   for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[i]) {
-        let smallerValue = arr[j];
-        arr[j] = arr[i];
-        arr[i] = smallerValue;
-        console.log(arr);
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j + 1] < arr[j]) {
+        let temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
       }
     }
   }
-  
   return arr;
 };
-
-console.log(bubbleSort([103, 5, 80, 3, 3, 340 , 8]));
-//[]
+console.log(bubbleSort([103, 5,5, 80, 3, 8]));
