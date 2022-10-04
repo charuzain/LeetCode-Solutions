111487a1cd7430d164cbf5498a88f96a317d120e
charuzain <charuabhizain@gmail.com>
2022-10-03 23:35:50 -0400

    reverse s string

--- challenges/reverseString.js
+++ challenges/reverseString.js
@@ -1,8 +1,19 @@
-// "Hi my name is Charu !!"
+//Write a function to reverse a string "Hi my name is Charu !!"
 
 const reverseString = function(str) {
-  console.log(str.split("").reverse().join(""));
+  return str.split("").reverse().join("");
 };
 
 
 console.log(reverseString("Hi my name is Charu !!"));
+
+
+const reverse = function(str) {
+  let reversedStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
+  return reversedStr;
+  
+};
+console.log(reverse("Hi my name is Charu !!"));
